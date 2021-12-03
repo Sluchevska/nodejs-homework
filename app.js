ae133bab7fcab589f118557a901be894f272344d
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-12-03 16:12:00 +0000

    token add

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const contactsRouter = require('./routes/api/contacts')
 
 const authRouter = require('./routes/api/auth')
 
+const usersRouter = require('./routes/api/users')
+
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
@@ -18,6 +20,8 @@ app.use(express.json())
 app.use('/api/users', authRouter)
 
 app.use('/api/contacts', contactsRouter)
+
+app.use('/api/users', usersRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const contacts = require('./contacts')
 const auth = require('./auth')
+const users = require('./users')
 
 module.exports = {
   contacts,
-  auth
+  auth,
+  users
 
 }
