699fde6f3c08cf5d60c0dcaa1c005b2274cb63a6
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-12-03 18:11:09 +0000

    logout

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -2,8 +2,14 @@ const { User } = require('../../models')
 
 const logout = async(req, res) => {
   const { _id } = req.user
+  const { email } = req.body
   await User.findByIdAndUpdate(_id, { token: null })
-  res.status(204).json()
+  res.status(204).json({
+    user: {
+      email,
+
+    },
+  })
 }
 
 module.exports = logout
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { controllerWrapper, validator } = require('../../middlewares')
+const { auth, controllerWrapper, validator } = require('../../middlewares')
 const { auth: ctrl } = require('../../controllers')
 const { joiRegisterSchema, joiLoginSchema } = require('../../models/users')
 
 router.post('/signup', validator(joiRegisterSchema), controllerWrapper(ctrl.register))
 router.post('/login', validator(joiLoginSchema), controllerWrapper(ctrl.login))
+router.get('/logout', auth, controllerWrapper(ctrl.logout))
+
 module.exports = router
