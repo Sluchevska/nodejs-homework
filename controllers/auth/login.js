7a10681b281b169934a13ee980a365b2b1715b1f
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-12-03 17:24:31 +0000

    add owner

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -6,7 +6,6 @@ const { SECRET_KEY } = process.env
 
 const login = async (req, res) => {
   const { email, password } = req.body
-
   const user = await User.findOne({ email })
   if (!user || !user.comparePassword(password)) {
     throw new Unauthorized('Email or password is wrong')
@@ -15,12 +14,12 @@ const login = async (req, res) => {
     id: user._id,
   }
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' })
-  const { subscription } = await User.findByIdAndUpdate(user._id, { token })
+  await User.findByIdAndUpdate(user._id, { token })
   res.json({
     user: {
       token,
       email,
-      subscription,
+      subscription: user.subscription,
     },
   })
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { controllerWrapper, validator } = require('../../middlewares')
+const { auth, controllerWrapper, validator } = require('../../middlewares')
 const { joiSchema, statusJoiSchema } = require('../../models/contacts')
 const { contacts: ctrl } = require('../../controllers')
 
-router.get('/', controllerWrapper(ctrl.getAll))
+router.get('/', auth, controllerWrapper(ctrl.getAll))
 router.get('/:contactId', controllerWrapper(ctrl.getById))
-router.post('/', validator(joiSchema), controllerWrapper(ctrl.add))
+router.post('/', auth, validator(joiSchema), controllerWrapper(ctrl.add))
 
 router.put(
   '/:contactId',
